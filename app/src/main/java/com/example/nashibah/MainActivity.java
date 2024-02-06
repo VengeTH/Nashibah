c2b058a7d15997e4d80280a774b5655c30c36a4f
VengeTH <96782670+VengeTH@users.noreply.github.com>
2024-02-06 16:30:27 +0800

    ongoing apology project for my love

---
+++ app/src/main/java/com/example/nashibah/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.nashibah;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.press);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, next.class));
+                overridePendingTransition(R.anim.fade_in, 0);
+            }
+        });
+    }
+}
