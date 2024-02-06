a30f2579d7fe5441c5b8ba2a66482e59dd02cd37
VengeTH <96782670+VengeTH@users.noreply.github.com>
2024-02-06 19:25:15 +0800

    dito na muna, nagana na eh

--- app/src/main/java/com/example/nashibah/MainActivity.java
+++ app/src/main/java/com/example/nashibah/MainActivity.java
@@ -1,25 +1,64 @@
 package com.example.nashibah;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private MusicPlayerService musicPlayerService;
+    private boolean isServiceBound = false;
+
+    private ServiceConnection serviceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            MusicPlayerService.MusicPlayerBinder binder = (MusicPlayerService.MusicPlayerBinder) service;
+            musicPlayerService = binder.getService();
+            isServiceBound = true;
+            musicPlayerService.start(); // Start music playback when the service is connected
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            isServiceBound = false;
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "Turn up the volume, lovey", Toast.LENGTH_SHORT).show();
+
+        // Bind to the MusicPlayerService
+        Intent intent = new Intent(this, MusicPlayerService.class);
+        bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE);
 
         Button button = findViewById(R.id.press);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // Start the next activity with a fade-in animation
                 startActivity(new Intent(MainActivity.this, next.class));
-                overridePendingTransition(R.anim.fade_in, 0);
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
             }
         });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // Unbind from the MusicPlayerService
+        if (isServiceBound) {
+            unbindService(serviceConnection);
+            isServiceBound = false;
+        }
     }
 }
